609c02ab14a09f75f5aa8dbc5d9d5645f05e3c24
SimonWT <y.yudinskikh@innopolis.ru>
2020-07-18 15:25:02 +0400

    First commit

---
+++ nuxt.config.js
@@ -0,0 +1,98 @@
+export default {
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: 'universal',
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: 'server',
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
+    ],
+    link: [
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons',
+      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+        integrity:
+          'sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk',
+        crossorigin: 'anonymous',
+      },
+    ],
+    script: [
+      {
+        src:
+          'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js',
+      },
+      { src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js' },
+      {
+        src:
+          'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js',
+      },
+    ],
+  },
+  /*
+   ** Global CSS
+   */
+  css: ['element-ui/lib/theme-chalk/index.css'],
+  /*
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: ['@/plugins/element-ui'],
+  /*
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
+  components: true,
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    '@nuxtjs/pwa',
+    [
+      '@nuxtjs/yandex-metrika',
+      {
+        id: 'XXXXXX',
+        webvisor: true,
+        // clickmap:true,
+        // useCDN:false,
+        // trackLinks:true,
+        // accurateTrackBounce:true,
+      },
+    ],
+  ],
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h1 class="mb-5">Presents for your Pair!</h1>
+        </div>
+        <div class="col-12 text-center">
+          <h2 class="mb-4">Roses</h2>
+        </div>
+        <div v-for="i in 22" :key="i" class="col-12 col-md-4 col-lg-4">
+          <img
+            class="w-100 mb-3"
+            :src="require(`~/assets/img/${i}.JPG`)"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<style scoped>
+.content {
+  margin: 0 auto;
+  max-width: 768px;
+}
+
+.sidebar {
+  min-width: 300px;
+}
+</style>
